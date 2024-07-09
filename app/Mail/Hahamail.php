d13925ceeb6f1c69640ddbc156714376d6cd13ac
habib-hb <developerhabib1230@gmail.com>
2024-07-09 11:20:22 +0600

    wow

---
+++ app/Mail/Hahamail.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class Hahamail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+     // Public function available to the blade file
+     public $data;
+    public function __construct($data)
+    {
+        // Defining the data that will be passed to the blade file
+        $this->data = $data;
+    }
+
+
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            // from: new Address('jeffrey@example.com', 'Jeffrey Way'),
+            subject: 'Hahamail',
+        );
+    }
+
+
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
